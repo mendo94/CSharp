af6a72e00b555df4f66aac8488d87cf5c016ede9
Rina Mendoza <rinamendozaa@gmail.com>
2022-07-20 21:51:23 -0400

    position will now be where touch press it

---
+++ Assets/Scripts/BallHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+    [SerializedField] private Rigidbody2D currentBallRigidbody;
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            return;
+        }
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidbody.position = worldPosition;
+    }
+}
